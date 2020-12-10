e4e197fa674a4c4da3b9f7d07461b0ca4fce807c
kencollier3 <mckennoncollier@gmail.com>
2020-12-10 13:31:48 -0700

    update

--- ReadingInput/src/com/company/Main.java
+++ ReadingInput/src/com/company/Main.java
@@ -7,15 +7,23 @@ public class Main {
     public static void main(String[] args) {
 	    Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your year of birth: ");
-        int yearBirth = scanner.nextInt();
-        scanner.nextLine();
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int yearBirth = scanner.nextInt();
+            scanner.nextLine();
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2020 - yearBirth;
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2020 - yearBirth;
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
-
+            if (age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth.");
+            }
+        } else {
+            System.out.println("Please enter a number.");
+        }
 
         scanner.close();
     }
