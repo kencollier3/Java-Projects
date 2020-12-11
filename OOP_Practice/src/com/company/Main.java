87a13c85f579f49d5e5eea00345bb20dd2ef4031
kencollier3 <mckennoncollier@gmail.com>
2020-12-11 11:17:42 -0700

    start of OOP practice

---
+++ OOP_Practice/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car porsche = new Car(2, 4, "Carerra", "v8", "blue");
+	    Car honda = new Car(4, 4, "Civic", "v4", "gray");
+    }
+}
